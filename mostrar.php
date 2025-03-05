c96c6d8acbc4fad9f5603ea5c1738192ab2fb40c
IsmaelCamarillo <icamarillo041@gmail.com>
2025-03-04 19:31:47 -0700

    mostrar.php

--- mostrar.php
+++ mostrar.php
@@ -1,11 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+// Conexión a la base de datos
+$servername = "fdb1028.awardspace.net";
+$username = "4595736_validacion"; 
+$password = "1234567890a"; 
+$dbname = "4595736_validacion"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+
+
+// Consultar datos desencriptados (sin IP)
+$sql = "SELECT id, 
+               CAST(AES_DECRYPT(first_name, 'clave_secreta') AS CHAR) AS first_name, 
+               CAST(AES_DECRYPT(last_name, 'clave_secreta') AS CHAR) AS last_name 
+        FROM usuarios";
+$result = $conn->query($sql);
+?>
+
+<!-- Tabla para mostrar los datos desencriptados -->
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+    </tr>
+    <?php
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+                    <td>".$row["id"]."</td>
+                    <td>".$row["first_name"]."</td>
+                    <td>".$row["last_name"]."</td>
+                  </tr>";
+        }
+    } else {
+        echo "<tr><td colspan='3'>No hay datos en la base de datos</td></tr>";
+    }
+    $conn->close();
+    ?>
+</table>
